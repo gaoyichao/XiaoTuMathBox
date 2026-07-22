2198148274775b2fb8163fb3c7934aab46016e2b
GAO.YiChao <xiaotu@gaoyichao.com>
2026-07-22 23:25:01 +0800

    [featureDev] add NewtonRaphson

--- include/XiaoTuMathBox/Numerical/EquationRoot.hpp
+++ include/XiaoTuMathBox/Numerical/EquationRoot.hpp
@@ -11,6 +11,10 @@ namespace xiaotu {
         return (T(0) < val) - (val < T(0));
     }
 
+    //! @brief 二分法求解方程 f(x) = 0 在闭区间 [a, b] 上的根
+    //!
+    //! @param [in] max_iter 最大迭代次数
+    //! @param [in] tol 终止迭代时的区间长度
     template <typename DataType>
     DataType Bisection(std::function<DataType(DataType)> f, DataType a, DataType b,
                        int max_iter = 100, DataType tol = SMALL_VALUE)
@@ -44,6 +48,33 @@ namespace xiaotu {
 
         return re;
     }
+
+
+    template <typename DataType>
+    DataType NewtonRaphson(std::function<DataType(DataType)> f,
+                           std::function<DataType(DataType)> df, 
+                           DataType x0,
+                           int max_iter = 100, DataType tol = SMALL_VALUE)
+    {
+        DataType re = x0;
+        for (int i = 0; i < max_iter; ++i) {
+            DataType y = f(re);
+            if (0 == y)
+                return re;
+
+            DataType dydx = df(re);
+            assert(0 != dydx); // 是否需要 std::abs(dfdx) < SMALL_VALUE ?
+
+            re = x0 - y / dydx;
+            if (std::abs(re - x0) < tol)
+                return re;
+
+            x0 = re;
+        }
+
+        return re;
+    }
+
 
 }
 
--- test/MathBox/Numerical/t_EquationRoot.cpp
+++ test/MathBox/Numerical/t_EquationRoot.cpp
@@ -3,18 +3,69 @@
 #include <XiaoTuDataBox/Utils.hpp>
 #include <XiaoTuMathBox/Numerical/EquationRoot.hpp>
 
+#include <cmath>
 #include <gtest/gtest.h>
-
-double douniwan(double x)
-{
-    return 0.5 * x;
-}
 
 
 TEST(EquationRoot, Bisection)
 {
-    double root = xiaotu::Bisection<double>(douniwan, -1.0, 1.0);
-    XTLog(std::cout) << root << std::endl;
+    {
+        double root = xiaotu::Bisection<double>(
+        [](double x) {
+            return 0.5 * x;
+        }, -1.0, 1.0);
+        EXPECT_TRUE(std::abs(root - 0) < SMALL_VALUE);
+    }
+
+    {
+        double root = xiaotu::Bisection<double>(
+        [](double x) {
+            return x * x - 4;
+        }, 0.0, 3.0, 100, 1e-20);
+        EXPECT_DOUBLE_EQ(root, 2);
+    }
 
 }
 
+
+TEST(EquationRoot, NewtonRaphson)
+{
+    {
+        double root = xiaotu::NewtonRaphson<double>(
+        [](double x) {
+            return 0.5 * x;
+        }, 
+        [](double x) {
+            return 0.5;
+        }, 
+        1.0);
+        EXPECT_TRUE(std::abs(root - 0) < SMALL_VALUE);
+    }
+
+    {
+        double root = xiaotu::NewtonRaphson<double>(
+        [](double x) {
+            return x * x - 4;
+        }, 
+        [](double x) {
+            return 2 * x;
+        }, 
+        1.0, 100, 1e-20);
+        EXPECT_DOUBLE_EQ(root, 2);
+    }
+
+
+    {
+        double root = xiaotu::NewtonRaphson<double>(
+        [](double x) {
+            return std::cos(x) - x;
+        }, 
+        [](double x) {
+            return -std::sin(x) - 1;
+        }, 
+        0.0, 100, 1e-20);
+        EXPECT_TRUE(std::abs(root - 0.7390851332) < 1e-9);
+    }
+
+
+}
